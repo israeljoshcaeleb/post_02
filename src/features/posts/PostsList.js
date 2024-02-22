3e3164327d8add36a2b5f87aaa750ec57607aa3e
israeljoshcaeleb <izael112212@gmail.com>
2024-02-22 14:24:35 +0800

    Implementing a post to an endpoint

---
+++ src/features/posts/PostsList.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchPosts, getPostsError, getPostsStatus, selectAllPosts } from './postsSlice'
+import PostsExcerpt from './PostsExcerpt'
+
+const PostsList = () => {
+    const dispatch = useDispatch()
+    const posts = useSelector(selectAllPosts)
+    const postStatus = useSelector(getPostsStatus)
+    const error = useSelector(getPostsError)
+
+    useEffect(() =>  {
+        if (postStatus === 'idle'){
+            dispatch(fetchPosts())
+        }
+    },[postStatus, dispatch])
+
+    let content
+    if (postStatus === 'loading'){
+        content = <p>Loading...</p>
+    } else if (postStatus === 'succeeded'){
+        const orderedPosts  = posts.slice().sort((a, b)  => b.date.localeCompare(a.date))
+        content = orderedPosts.map(post => <PostsExcerpt key={post.id} post={post} />)
+    } else  if (postStatus === 'failed'){
+        content = <p>{error}</p>
+    }
+  return (
+    <section>
+        <h2>Posts</h2>
+        {content}
+    </section>
+  )
+}
+
+export default PostsList
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import { fetchUsers } from './features/users/usersSlice';
+import { fetchPosts } from './features/posts/postsSlice';
+
+store.dispatch(fetchUsers());
+// store.dispatch(fetchPosts())
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <App />
+    </Provider>
+  </React.StrictMode>
+);
+
